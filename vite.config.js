306f5b8c4d76fdc7683d31fe48410b2cfb67e2ab
Kurovin <rovinocampoo@gmail.com>
2025-03-17 17:52:23 +0800

    last thing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/portfolio-website/",
+  base: "/portfolio-website/", //base added
   build: {
     outDir: 'dist', // Ensure correct output directory
     assetsDir: 'assets', // Ensure assets go into a subdirectory
